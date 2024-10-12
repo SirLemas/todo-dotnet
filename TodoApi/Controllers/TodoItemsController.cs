85265089f5f912aa8f2919854fa0f10685e86b7e
Talles Nogueira <t-alles-1@hotmail.com>
2024-10-12 20:07:24 -0300

    Adição de controler pelo middleware para as rotas serem acessadas somente com token

--- TodoApi/Controllers/TodoItemsController.cs
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Contexts;
@@ -11,35 +7,48 @@ using TodoApi.Services;
 
 namespace TodoApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
         private readonly TodoContext _context;
+        private readonly UserContext _userContext;
         private readonly TodoItemValidationService _validate;
 
-        public TodoItemsController(TodoContext context, TodoItemValidationService validateService)
+        public TodoItemsController(TodoContext context, UserContext userContext, TodoItemValidationService validateService)
         {
             _context = context;
             _validate = validateService;
+            _userContext = userContext;
+        }
+
+        private async Task<User> GetLoggedUser()
+        {
+            string userEmail = User.Claims.First(c => c.Type == "Email").Value;
+            return await _userContext.Users.FirstOrDefaultAsync(u => u.Email == userEmail);
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
         {
             var todoItems = await _context.TodoItems.ToListAsync();
-            return new JsonResult(todoItems);
+            return new JsonResult(todoItems) {StatusCode = StatusCodes.Status200OK};
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
         {
-            var todoItem = await _context.TodoItems.FindAsync(id);
+            var user = GetLoggedUser();
+            if (user == null) {
+                return new JsonResult(new { message = "O usuário não foi encontrado"}) { StatusCode = StatusCodes.Status404NotFound };
+            }
+
+            var todoItem = await _context.TodoItems.FirstOrDefaultAsync(t => t.Id == id && t.UserId == user.Id);
 
             if (todoItem == null)
             {
-                return new JsonResult(new { message = "Nenhum item foi encontrado" }) { StatusCode = StatusCodes.Status404NotFound };
-                // return NotFound("Nenhum item foi encontrado");
+                return new JsonResult(new { message = "Nenhum item foi encontrado"}) { StatusCode = StatusCodes.Status404NotFound };
             }
 
             return new JsonResult(todoItem);
@@ -50,7 +59,7 @@ namespace TodoApi.Controllers
         {
             if (id != todoItem.Id)
             {
-                return new JsonResult(new { message = "ID passado não corresponde ao item a ser atualizado, tente novamente!" }) { StatusCode = StatusCodes.Status400BadRequest };
+                return new JsonResult(new { message = "ID passado não corresponde ao item a ser atualizado, tente novamente!"}) { StatusCode = StatusCodes.Status400BadRequest };
             }
 
             var errors = _validate.ValidateTodoItem(todoItem);
@@ -59,9 +68,22 @@ namespace TodoApi.Controllers
                 return new JsonResult(new {errors}) {StatusCode = StatusCodes.Status400BadRequest};
             }
 
-            _context.TodoItems.Update(todoItem);
-            await _context.SaveChangesAsync();
-            return new JsonResult(new { message = "Item atualizado com sucesso!" });
+            try
+            {
+                var user = GetLoggedUser();
+                if (user == null) {
+                    throw new NullReferenceException("O usuário não foi encontrado para este item");
+                }
+
+                todoItem.UserId = user.Id;
+                _context.TodoItems.Update(todoItem);
+                await _context.SaveChangesAsync();
+                return new JsonResult(new { message = "Item atualizado com sucesso!"}) {StatusCode = StatusCodes.Status200OK};
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new {message = $"Aconteceu algum erro inesperado\nErro: {ex.Message}"}) {StatusCode = StatusCodes.Status400BadRequest};
+            }
         }
 
         [HttpPost]
@@ -74,10 +96,22 @@ namespace TodoApi.Controllers
                 return new JsonResult(new {errors}) {StatusCode = StatusCodes.Status400BadRequest};
             }
 
-            _context.TodoItems.Add(todoItem);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var user = GetLoggedUser();
+                if (user == null) {
+                    throw new NullReferenceException("O usuário não foi encontrado para este item");
+                }
 
-            return new JsonResult(new {message = "Item criado com sucesso!"}) { StatusCode = StatusCodes.Status201Created };
+                todoItem.UserId = user.Id;
+                _context.TodoItems.Add(todoItem);
+                await _context.SaveChangesAsync();
+                return new JsonResult(new {message = "Item criado com sucesso!"}) { StatusCode = StatusCodes.Status201Created};
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new {message = $"Aconteceu algum erro inesperado\nErro: {ex.Message}"}) { StatusCode = StatusCodes.Status400BadRequest};
+            }
         }
 
         [HttpDelete("{id}")]
